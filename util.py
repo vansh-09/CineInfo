bfae6ee8148062c60fffe4891e31b45e70c44a72
vanshjainx <vanshkjain09@gmail.com>
2025-06-09 14:57:00 +0530

    Update util.py

--- util.py
+++ util.py
@@ -1,13 +1,1 @@
-import requests
 
-def fetch_movie_data(movie_name):
-    api_key = "517abc65"  # Already noted
-    url = f"http://www.omdbapi.com/?t={movie_name}&apikey={api_key}"
-
-    response = requests.get(url)
-    data = response.json()
-
-    if data.get("Response") == "True":
-        return data
-    else:
-        return None
